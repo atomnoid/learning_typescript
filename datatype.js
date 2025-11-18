08a77a96b2b676693c60a2cb92923a3819b17210
atomnoid <aayushs2580@gmail.com>
2025-11-18 21:37:16 +0530

    js file

--- datatype.js
+++ datatype.js
@@ -1,3 +1,4 @@
+"use strict";
 // basic types
 // primitive types (number, string, boolean, null, undefined, symbol, bigint)
 // array types
@@ -5,13 +6,14 @@
 // enum types
 // any type
 // void type, null, undefined types, never type, object type, void type
+Object.defineProperty(exports, "__esModule", { value: true });
 // primitive types and reference types
 var a = 10; // number
 var b = "hello"; // string
 var c = true; // boolean
 var d = null; // null
 var e = undefined;
-var f = Symbol("sym"); // symbol
+var z = Symbol("sym"); // symbol
 var g = 9007199254740991n; // bigint
 var h = [1, 2, 3]; // array
 var i = [1, "two", true]; // tuple
@@ -27,7 +29,7 @@ function logMessage() {
     console.log("This function returns nothing");
 }
 var l = { name: "Alice", age: 30 }; // object 
-var arr = [12, "hello",];
+let arr = [12, "hello",];
 var numberM;
 (function (numberM) {
     numberM[numberM["NUM"] = 1] = "NUM";
@@ -35,3 +37,4 @@ var numberM;
 })(numberM || (numberM = {}));
 numberM.NUM;
 // it is like a help book where you can find the data you have mentioned before in the code.
+//# sourceMappingURL=datatype.js.map
