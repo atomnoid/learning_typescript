1ef4da924aa2f0ac640f08db7c953c554f02b9ff
atomnoid <aayushs2580@gmail.com>
2025-11-20 02:08:28 +0530

    learned about constructors and objs

--- classandobjs.ts
+++ classandobjs.ts
@@ -1,4 +1,4 @@
-// classes and objects 
+// classes and objects
 // class definition
 // constructors
 // access modifiers (public, private, protected)
@@ -9,32 +9,43 @@
 // static members, properties and methods
 // Abstract classes and methods
 
-
 class device {
-    name = "samsung";
-    price = 25000;
-    category = "mobile";
+  name = "samsung";
+  price = 25000;
+  category = "mobile";
 }
 
 let d1 = new device();
 let d2 = new device();
 
-
 // constructors is a special method that is called when an object is instantiated from a class
 
 class bottlemaker {
-    constructor(public name: string, public price: number){
-    }
-
+  constructor(public name: string, public price: number) {}
 }
 let b1 = new bottlemaker("milton", 500);
 
-
-class humanmaker{
-    age = 100;
-    constructor(public name: string, public ishandsome: boolean){
-    }
+class humanmaker {
+  age = 0;
+  constructor(public name: string, public ishandsome: boolean) {}
 }
 
-let h1 = new humanmaker("john", true); 
+let h1 = new humanmaker("john", true);
 let h2 = new humanmaker("doe", false);
+
+class music {
+  constructor(
+    public title: string,
+    public artist: string,
+    public duration: number,
+    public thumbnail: string = "thumbnail.jpg",
+    public free: boolean
+  ) {
+    if (!thumbnail) {
+      this.thumbnail = "thumbnail.jpg";
+    }
+  }
+}
+
+let music1 = new music("let it happen", "tame impala", 320, undefined, true);
+let music2 = new music("boderline", "tame impala", 290, "mymusic.jpg", true);
