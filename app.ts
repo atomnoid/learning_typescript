75f64cdaa09ab6579814d3bc28c02a476fac05d3
atomnoid <aayushs2580@gmail.com>
2025-11-20 19:15:19 +0530

    none

--- app.ts
+++ app.ts
@@ -2,3 +2,16 @@ var a = 23;
 function f() {
     return a;
 }
+
+
+class me{
+    constructor(public name : string, public age: number, public geder?: string){
+        
+    }
+    changename(){
+        this.name = "aayush";
+    }
+}
+
+let name1 = new me("john", 23);
+
