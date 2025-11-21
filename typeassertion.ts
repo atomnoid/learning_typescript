d578fcdbad640f8a4b6a45503b02faa8b451c111
atomnoid <aayushs2580@gmail.com>
2025-11-21 16:45:36 +0530

    learned about type assertions

---
+++ typeassertion.ts
@@ -0,0 +1,24 @@
+// Type assettion
+// Type casting
+// non-null assertion operator
+
+
+let a: any = 12;
+
+//(a as string).toLowerCase();
+
+(<number>a).toPrecision(); //another way of type assertion
+
+
+// Type casting 
+
+let num = Number("12");
+console.log(num);
+
+// Non-null assertion operator
+
+let abc: null | undefined | string;
+
+a = "hello world";
+a!
+
